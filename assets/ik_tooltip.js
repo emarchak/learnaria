9f28774255b3c3df1e6f2349c042361578a3f183
emarchak <erin.marchak@shopify.com>
2018-10-21 21:53:08 -0400

    Activity 6: Tooltips Open.

--- assets/ik_tooltip.js
+++ assets/ik_tooltip.js
@@ -37,14 +37,20 @@
 				.text(tip)
 				.addClass('ik_tooltip')
 				.attr({
-					'id': id,
+          'id': id,
+          'role': 'tooltip', // assign tooltip role
+          'aria-hidden': 'true', // hide it from screen reader to prevent it from been read twice
+          'aria-live': 'polite' // make it live region
 				});
 			
 			$elem
-				.css('position', 'relative')
-				.removeAttr('title') // remove title to prevent it from being read
-				.after($tooltip)
-				.on('mouseover', function(event) {
+        .attr({
+          'tabindex': 0 // add tab order
+        })
+        .css('position', 'relative')
+        .removeAttr('title') // remove title to prevent it from being read
+        .after($tooltip)
+        .on('mouseover focus', function(event) {
 					
 					var y, x;
 					
@@ -62,6 +68,9 @@
 					}
 					
 					$tooltip // position and show tooltip
+            .attr({
+              'aria-hidden': 'false'
+            })
 						.css({
 							'top': y, 
 							'left': x
@@ -73,6 +82,9 @@
 					if (!$(event.currentTarget).is(':focus') ) { // hide tooltip if current element is not focused
 						
 						$tooltip
+              .attr({
+                'aria-hidden': 'true'
+              })
 							.removeClass('visible mouseover');					
 					}
 										
